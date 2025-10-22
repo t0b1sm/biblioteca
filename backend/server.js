b50df2ad3790c18ced0826e2f08098562a805956
Aluno SENAI <Aluno@SN-363453.edu.fiesc.com.br>
2025-10-22 15:58:46 -0300

    Tudo meio pronto da aula passada

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,16 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// // Test route
+// app.get("/", (req, res) => {
+//  res.json({ message: "API Biblioteca funcionando " });
+// });
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//  console.log("Servidor rodando em http://localhost:${PORT}");
+// });
---
+++ database/db.sql
@@ -0,0 +1,24 @@
+-- CREATE DATABASE Biblioteca;
+-- \c Biblioteca;
+-- CREATE TABLE usuarios (
+--  id SERIAL PRIMARY KEY,
+--  nome VARCHAR(100) NOT NULL,
+--  email VARCHAR(100) UNIQUE NOT NULL,
+--  senha VARCHAR(255) NOT NULL,
+--  criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+-- CREATE TABLE livros (
+--  id SERIAL PRIMARY KEY,
+--  titulo VARCHAR(200) NOT NULL,
+--  autor VARCHAR(100),
+--  isbn VARCHAR(20) UNIQUE,
+--  ano_publicacao INT,
+--  disponivel BOOLEAN DEFAULT TRUE
+-- );
+-- CREATE TABLE emprestimos (
+--  id SERIAL PRIMARY KEY,
+--  usuario_id INT REFERENCES usuarios(id),
+--  livro_id INT REFERENCES livros(id),
+--  data_emprestimo DATE DEFAULT CURRENT_DATE,
+--  data_devolucao DATE
+-- );
